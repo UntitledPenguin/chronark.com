ecfa152b75b1d694fe306494f60b8b0115b30e4d
UntitledPenguin <qramisw@gmail.com>
2024-08-04 01:44:32 +0100

    modified:   app/projects/page.tsx

--- app/projects/page.tsx
+++ app/projects/page.tsx
@@ -1,7 +1,6 @@
 import Link from "next/link";
 import React from "react";
 import Image from 'next/image';
-import Particles from "../components/particles";
 import { allProjects } from "contentlayer/generated";
 import { Navigation } from "../components/nav";
 import { Card } from "../components/card";
