02df4eb0b820ac1ebdce1865e4dcb238dab920a8
UntitledPenguin <qramisw@gmail.com>
2024-08-04 01:45:11 +0100

    modified:   app/projects/page.tsx

--- app/projects/page.tsx
+++ app/projects/page.tsx
@@ -1,6 +1,7 @@
 import Link from "next/link";
 import React from "react";
 import Image from 'next/image';
+import Particles from "../components/particles";
 import { allProjects } from "contentlayer/generated";
 import { Navigation } from "../components/nav";
 import { Card } from "../components/card";
@@ -49,6 +50,7 @@ export default async function ProjectsPage() {
         height={540}
         className="object-cover"
         />
+      </div>
 
       <div className="px-6 pt-20 mx-auto space-y-8 max-w-7xl lg:px-8 md:space-y-16 md:pt-24 lg:pt-32">
         <div className="max-w-2xl mx-auto lg:mx-0">
