0296b760bef5fa45e5cbcfefc34042b7c82f10f2
UntitledPenguin <qramisw@gmail.com>
2024-08-14 21:24:39 +0100

    modified:   app/projects/page.tsx 	new file:   content/projects/Documentation.mdx 	new file:   content/projects/Welcome.mdx 	new file:   content/projects/Workflow.mdx

--- app/projects/page.tsx
+++ app/projects/page.tsx
@@ -22,9 +22,9 @@ export default async function ProjectsPage() {
     return acc;
   }, {} as Record<string, number>);
 
-  const featured = allProjects.find((project) => project.slug === "unkey")!;
-  const top2 = allProjects.find((project) => project.slug === "planetfall")!;
-  const top3 = allProjects.find((project) => project.slug === "highstorm")!;
+  const featured = allProjects.find((project) => project.slug === "Welcome")!;
+  const top2 = allProjects.find((project) => project.slug === "Documentation")!;
+  const top3 = allProjects.find((project) => project.slug === "Workflow")!;
   const sorted = allProjects
     .filter((p) => p.published)
     .filter(
